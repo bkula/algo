186c8b8502498aee471022d4b66b50852f65f521
Benedykt Kula <benedykt.kula@gmail.com>
2016-02-24 10:33:26 +0100

    V*

--- grafy/lista_incydencji.cpp
+++ grafy/lista_incydencji.cpp
@@ -2,41 +2,52 @@
 #include <vector>
 using namespace std;
 
+struct V;
+
 struct E // edge
 {
-    E(int _to, int _w)
+    V* to;
+    int w; // weight
+
+    E(V* _to, int _w)
     : to(_to)
     , w (_w)
     {
     }
-
-    int to;
-    int w; // weight
 };
 
 struct V // vertex
 {
-    vector<E> e;
+    int id;
+    vector<E> e; // lista krawędzi
+
+    V()
+    : id(maxId++)
+    {
+    }
+
+    static int maxId; // ilość wierzchołków
 };
+
+int V::maxId = 0;
 
 int main()
 {
     int n, m; // wierzchołki, krawędzie
     cin >> n >> m;
 
-    V graf[n]; // skierowany, ważony
+    vector<V> graf(n); // skierowany, ważony
 
     for (int i = 0; i < m; i++) {
         int a, b, c;
         cin >> a >> b >> c;
-        a--, b--;
-        graf[a].e.push_back(E(b, c));
+        graf[a - 1].e.push_back(E(&graf[b - 1], c));
     }
 
     for (int i = 0; i < n; i++) {
         cout << i + 1 << ":";
         for (int j = 0; j < graf[i].e.size(); j++) {
-            cout << " " << graf[i].e[j].to + 1 << "(" << graf[i].e[j].w << ")";
+            cout << " " << graf[i].e[j].to->id + 1 << "(" << graf[i].e[j].w << ")";
         }
         cout << endl;
     }
